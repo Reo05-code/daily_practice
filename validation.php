70770b39cbf1b71cb51ad2f6a8b692645f16bbc2
Reo05-code <t.asaishi05@gmail.com>
2025-09-22 14:27:09 +0900

    Add files via upload

---
+++ validation.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP基礎編</title>
+</head>
+<body>
+   <p>
+      <?php
+      $name ='侍太郎';
+      $password = 12345678;
+      $email = 'samuraitarou@example.com';
+      $gender = '男性';
+
+      if (empty($name)) {
+         exit('氏名を入力してください。');
+      }
+
+      if (mb_strlen($password) < 8) {
+         exit('パスワードは8文字以上にしてください。');
+      }
+
+      if (!filter_var( $email, FILTER_VALIDATE_EMAIL)) {
+         exit('メールアドレスの入力形式が正しくありません。');
+      }
+
+      if (!in_array( $gender, ['男性', '女性', 'その他'])) {
+         exit('正しい性別を指定してください。');
+      }
+
+      echo 'バリデーションの結果、問題ありませんでした。'
+
+      ?>
+   </p>
+</body>
+</html>
