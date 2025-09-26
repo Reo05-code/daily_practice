5b47dc5e0c932eb977bc56c44860b8b7428f7553
Reo05-code <t.asaishi05@gmail.com>
2025-09-26 12:15:06 +0900

    Add files via upload

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = 'mysql:dbname=php_db;host=localhost; charset=utf8mb4';
+$dsn = 'mysql:dbname=php_db_app;host=localhost;charset=utf8mb4';
 $user = 'root';
 $password = '';
 
@@ -7,48 +7,61 @@ if (isset($_POST['submit'])) {
    try {
       $pdo = new PDO($dsn, $user, $password);
 
-      $sql = '
-         UPDATE users
-         SET name = :name,
-         furigana = :furigana,
-         email = :email,
-         age = :age,
-         address = :address
+      $sql_update = '
+         UPDATE products
+         SET product_code = :product_code,
+         product_name = :product_name,
+         price = :price,
+         stock_quantity = :stock_quantity,
+         vendor_code = :vendor_code
          WHERE id = :id
-      ';
-      $stmt = $pdo->prepare($sql);
+   ';
+   $stmt_update = $pdo->prepare($sql_update);
 
-      $stmt->bindValue(':name', $_POST['user_name'], PDO::PARAM_STR);
-      $stmt->bindValue(':furigana', $_POST['user_furigana'], PDO::PARAM_STR);
-      $stmt->bindValue(':email', $_POST['user_email'], PDO::PARAM_STR);
-      $stmt->bindValue(':age', $_POST['user_age'], PDO::PARAM_STR);
-      $stmt->bindValue(':address', $_POST['user_address'], PDO::PARAM_STR);
-      $stmt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+   $stmt_update->bindValue(':product_code', $_POST['product_code'], PDO::PARAM_INT);
+   $stmt_update->bindValue(':product_name', $_POST['product_name'], PDO::PARAM_STR);
+   $stmt_update->bindValue(':price', $_POST['price'], PDO::PARAM_INT);
+   $stmt_update->bindValue(':stock_quantity', $_POST['stock_quantity'], PDO::PARAM_INT);
+   $stmt_update->bindValue(':vendor_code', $_POST['vendor_code'], PDO::PARAM_INT);
 
-      $stmt->execute();
+   $stmt_update->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
 
-      header('Location: users.php');
-   } catch (PDOException $e) {
-      exit($e->getMessage());
-   }
+   $stmt_update->execute();
+
+   $count = $stmt_update->rowCount();
+
+   $message = "商品を{$count}件編集しました。";
+
+   header("Location: read.php?message={$message}");
+} catch (PDOException $e) {
+   exit($e->getMessage());
 }
+}
+
+
 
 if (isset($_GET['id'])) {
    try {
       $pdo = new PDO($dsn, $user, $password);
 
-      $sql = 'SELECT * FROM users WHERE id = :id';
-      $stmt = $pdo->prepare($sql);
+      $sql_select_product = 'SELECT * FROM products WHERE id = :id';
+      $stmt_select_product = $pdo->prepare($sql_select_product);
 
-      $stmt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+      $stmt_select_product->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
 
-      $stmt->execute();
+      $stmt_select_product->execute();
 
-      $user = $stmt->fetch(PDO::FETCH_ASSOC);
+      $product = $stmt_select_product->fetch(PDO::FETCH_ASSOC);
 
-      if ($user === FALSE) {
+      if ($product === FALSE) {
          exit('idパラメータの値が不正です。');
       }
+
+      $sql_select_vendor_codes = 'SELECT vendor_code FROM vendors';
+
+      $stmt_select_vendor_codes = $pdo->query($sql_select_vendor_codes);
+
+      $vendor_codes = $stmt_select_vendor_codes->fetchAll(PDO::FETCH_COLUMN);
    } catch (PDOException $e) {
       exit($e->getMessage());
    }
@@ -56,37 +69,67 @@ if (isset($_GET['id'])) {
    exit('idパラメータの値が存在しません。');
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>PHP+DB</title>
+   <title>商品管理アプリ</title>
    <link rel="stylesheet" href="css/style.css">
+
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
 </head>
 
 <body>
-   <h1>ユーザー更新</h1>
-   <p>更新する内容を入力してください。</p>
-   <form action="update.php?id=<?= $_GET['id'] ?>" method="post">
-      <div>
-         <label for="user_name">お名前<span>【必須】</span></label>
-         <input type="text" id="user_name" name="user_name" value="<?= $user['name'] ?>"  maxlength="60" required>
-
-         <label for="user_furigana">ふりがな<span>【必須】</span></label>
-         <input type="text" id="user_furigana" name="user_furigana" value="<?= $user['furigana'] ?>"maxlength="60" required>
-
-         <label for="user_email">メールアドレス<span>【必須】</span></label>
-         <input type="text" id="user_email" name="user_email" value="<?= $user['email'] ?>" maxlength="255" required>
-
-         <label for="user_age">年齢</label>
-         <input type="text" id="user_age" name="user_age" value="<?= $user['age'] ?>" min="13" max="130" required>
-
-         <label for="user_address">住所</label>
-         <input type="text" id="user_address" name="user_address" value="<?= $user['address'] ?>"maxlength="255" required>
+   <header>
+      <nav>
+         <a href="index.php">商品管理アプリ</a>
+      </nav>
+   </header>
+   <main>
+      <article class="registration">
+         <h1>商品編集</h1>
+         <div class="back">
+            <a href="read.php" class="btn">&lt;戻る</a>
          </div>
-         <button type="submit" name="submit" value="insert">更新</button>
-   </form>
+         <form action="update.php?id=<?= $_GET['id'] ?>" method="post" class="registration-form">
+            <div>
+               <label for="product_code">商品コード</label>
+               <input type="number" id="product_code" name="product_code" value="<?=$product['product_code']?>" min="0" max="100000000" required>
+
+               <label for="product_name">商品名</label>
+               <input type="text" id="product_name" name="product_name" value="<?=$product['product_name']?>" maxlength="50" required>
+
+               <label for="price">単価</label>
+               <input type="number" id="price" name="price" value="<?=$product['price']?>" min="0" max="100000000" required>
+
+               <label for="stock_quantity">在庫数</label>
+               <input type="number" id="stock_quantity" name="stock_quantity" value="<?=$product['stock_quantity']?>" min="0" max="100000000" required>
+
+               <label for="vendor_code">仕入先コード</label>
+               <select  id="vendor_code" name="vendor_code" required>
+                  <option disabled selected value>選択してください</option>
+                  <?php
+                  foreach ($vendor_codes as $vendor_code) {
+                     if ($vendor_code === $product['vendor_code']) {
+                        echo "<option value='{$vendor_code}' selected>{$vendor_code}</option>";
+                     } else {
+                        echo "<option value='{$vendor_code}'>{$vendor_code}</option>";
+                     }
+                  }
+                  ?>
+               </select>
+            </div>
+            <button type="submit" class="submit-btn" name="submit" value="update">更新</button>
+         </form>
+      </article>
+   </main>
+   <footer>
+      <p class="copyright">&copy; 商品管理アプリ All rights reserved.</p>
+   </footer>
 </body>
-</html>
+
+</html
